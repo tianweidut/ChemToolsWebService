eb798469cd866b362c6763a4e5acab8ada3a0c29
tianweidut <liutianweidlut@qq.com>
2013-01-13 11:28:11 +0800

    change js structure

---
+++ media/js/features.js
@@ -0,0 +1,14 @@
+/**
+ * @author tianwei
+ * the whole features js
+ */
+
+// tooltips
+$(document).ready(function(){
+  $("[rel=tooltip]").tooltip();
+});
+
+// carousel
+$(document).ready(function(){
+  $('#myCarousel').carousel();
+});
