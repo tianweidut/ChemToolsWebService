ac868c3f51d7f08942279e33397902a2b5450faf
tianweidut <liutianweidlut@qq.com>
2013-03-23 21:37:29 +0900

    adjust id and rel in commit page and finish raw_content js

--- media/js/calculate_submit.js
+++ media/js/calculate_submit.js
@@ -3,4 +3,14 @@
  * the 
  */
 
-// tooltips
+// test code
+$('#task_name_id1').change(function(){
+  $('#task_name_commit').text($(this).val());
+});
+
+
+//raw content copy
+$("[rel='raw_content']").change(function(){
+  var name = $(this).attr('id') + "_copy";
+  $("#"+name).text($(this).val());
+});
