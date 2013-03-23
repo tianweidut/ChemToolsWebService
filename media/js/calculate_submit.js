999545d651599ed4bfae91296188ab5e783cf651
tianweidut <liutianweidlut@qq.com>
2013-03-23 22:09:54 +0900

    code raw_choice

--- media/js/calculate_submit.js
+++ media/js/calculate_submit.js
@@ -2,15 +2,25 @@
  * @author tianwei
  * the 
  */
-
-// test code
-$('#task_name_id1').change(function(){
-  $('#task_name_commit').text($(this).val());
+$(document).ready(function(){
+  $('#response_type_copy > p').hide();
 });
-
 
 //raw content copy
 $("[rel='raw_content']").change(function(){
   var name = $(this).attr('id') + "_copy";
   $("#"+name).text($(this).val());
+});
+
+//raw choice copy
+$("[rel='raw_choice']").change(function(){
+  var name = $(this).attr("id") + "_selected";
+  if($(this).attr("checked") == "checked")
+    {
+      $("#" + name).show();
+    }
+  else
+    {
+      $("#" + name).hide();
+    }
 });
