c99b4c9c4b42d7f6c1e3800ed5595e86db95b6cf
tianweidut <liutianweidlut@qq.com>
2013-03-25 20:03:33 +0900

    finish hello world program for dajax

---
+++ gui/ajax.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 2013-03-25
+
+@author: tianwei
+
+Desc: This module will be used for ajax request, such as form valid, search
+      query, calculated submit.
+'''
+
+import simplejson
+from dajaxice.decorators import dajaxice_register
+
+
+@dajaxice_register(method='GET')
+@dajaxice_register(method='POST', name="calculate_submit_post")
+def calculate_submit(request, data):
+    return simplejson.dumps({'message': 'tianwei hello world!'+data})
