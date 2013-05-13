60dd56ab87f5f414b58a21c21fb69468618fc552
tianweidut <liutianweidlut@qq.com>
2013-05-13 16:26:33 +0900

    finish ui fix, we can close issue #40

--- urls.py
+++ urls.py
@@ -10,6 +10,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 
