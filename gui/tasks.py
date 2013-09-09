7aa75ac280b9f58102927dc148016b4f6f8e8cf5
tianweidut <liutianweidlut@qq.com>
2013-09-09 15:32:15 +0800

    change the T

--- gui/tasks.py
+++ gui/tasks.py
@@ -185,7 +185,7 @@ def calculateTask(task, model_name,arguments=None):
 
     try:
         print "---T----",arguments
-        if not arguments:
+        if arguments=='none':
             arguments='25'
         pm =PredictionModel([get_ModelName(model_name)],para,filepath,float(arguments.encode('utf-8')))
         if task.file_obj.file_type=='mol':
