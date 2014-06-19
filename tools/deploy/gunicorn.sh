f9ed1c4ef228d4233a7cfaa92d33379a5861d125
liutianwei <liutianwei@douban.com>
2014-06-19 22:12:24 +0800

    use gunicorn conf

--- tools/deploy/gunicorn.sh
+++ tools/deploy/gunicorn.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
 set -e
-LOGFILE=/var/chemistry/log/gunicorn.log
-LOGDIR=$(dirname $LOGFILE)
-NUM_WORKERS=4
-SOCK_FILE=/tmp/chemistry_appserver.sock
-
-# user/group to run as
-USER=est863
-GROUP=est863
 test -d $LOGDIR || mkdir -p $LOGDIR
 
 cd /var/chemistry/code
 . VENV/bin/activate
 
-gunicorn wsgi:application -w $NUM_WORKERS \
-    --bind=unix:$SOCK_FILE \
-    --worker-class=sync \
-    --worker-connection=1000 \
-    --timeout=30 \
-    --graceful-timeout=30 \
-    --name=chemistry-appserver \
-    --user=$USER --group=$GROUP \
-    --log-level=debug \
-    --log-file=$LOGFILE 2>>$LOGFILE \
-    
+gunicorn wsgi:application -c appserver_config.py 
