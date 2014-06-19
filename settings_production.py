b4906f78eabbfb751e3a722ea7bed8a905e3ae49
tianweidut <liutianweidlut@qq.com>
2014-06-19 20:53:01 +0800

    fix confi

--- settings_production.py
+++ settings_production.py
@@ -11,7 +11,7 @@ DATABASES = {
         'NAME': 'Chemistry',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
