b680763056a7fc56f520e3aff84cf79fae4a0102
tianweidut <liutianweidlut@qq.com>
2014-07-17 23:18:06 +0800

    update calcore test

--- chemistry/calcore/tests/test_cal.py
+++ chemistry/calcore/tests/test_cal.py
@@ -6,8 +6,8 @@ from chemistry.calcore.prediciton_model import prediction_model_calculate
 def calcore_validate():
     model = 'logKOA'
     smile = 'COc1ccccc1NC(=O)CC(=O)C'
-    mol_path = '/home/vagrant/tianwei/ChemToolsWebService/media/tmp/process_file/89db0959-9742-418c-a5d4-1c2906c1c337.mol'
-    temperature = 10.0
+    mol_path = ''
+    temperature = 25
 
     r = prediction_model_calculate(model, smile, mol_path, temperature)
     print r
