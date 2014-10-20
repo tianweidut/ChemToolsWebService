778107be50ff3a249b4ed5e19ca2947f44b6cf1b
liutianwei <liutianwei@douban.com>
2014-10-20 22:51:46 +0800

    remove pdf, use html to render email context

--- chemistry/tasks.py
+++ chemistry/tasks.py
@@ -5,15 +5,15 @@ import datetime
 
 from celery.decorators import task
 from django.conf import settings
-from django.core.files import File
 from django.contrib.sites.models import Site
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template import loader, Context
 
 from chemistry.calcore.prediciton_model import prediction_model_calculate
 from chemistry.models import SingleTask, SuiteTask, StatusCategory
 from chemistry import (STATUS_WORKING, STATUS_SUCCESS, STATUS_FAILED,
                        TASK_SUITE, TASK_SINGLE)
-from chemistry.util import generate_mol_image, generate_pdf
+from chemistry.util import generate_mol_image, suitetask_details
 from utils import chemistry_logger
 
 LOCK_EXPIRE = 60 * 5  # Lock expires in 5 minutes
@@ -64,12 +64,6 @@ def add_counter(suite_id):
     if finished_count == suite.total_tasks:
         suite.has_finished_tasks = suite.total_tasks
         suite.status_id = StatusCategory.objects.get(category=STATUS_SUCCESS)
-        try:
-            file_path = generate_pdf(id=suite_id, task_type=TASK_SUITE)
-            f = File(open(file_path))
-            suite.result_pdf = f
-        except Exception:
-            chemistry_logger.exception("failed to generate pdf")
 
         # send email
         send_email_task.delay(email=suite.email, sid=suite.sid)
@@ -80,29 +74,28 @@ def add_counter(suite_id):
     suite.save()
 
 
-@task()
-def send_email_task(email, sid):
-    """
-    send result email to user
-    """
-    subject = "EST863 Calculate WebService Notification Emails"
-    reports_list = ""
+def render_calculate_result_as_html(sid):
     site = Site.objects.get()
-    suitetask = SuiteTask.objects.get(sid=sid)
-    if suitetask.result_pdf:
-        reports_list += "http://" + site.domain + \
-            suitetask.result_pdf.url + "\n\r"
-    task_lists = SingleTask.objects.filter(sid=sid)
-    for task in task_lists:
-        if task.result_pdf:
-            reports_list += "http://" + site.domain + \
-                task.result_pdf.url + "\n\r"
+    t = loader.get_template("email_result.html")
+    data = suitetask_details(sid)
+    data.update(domain=site.domain)
+    c = Context(data)
+    return t.render(c)
 
-    message = "Congratulations! Your calculate task is Finished, please, check\
-               the reports\n%s" % reports_list
 
-    send_mail(subject, message,
-              settings.DEFAULT_FROM_EMAIL, [email])
+#@task()
+def send_email_task(observers, sid):
+    subject = "计算结果结果邮件-化学品预测毒理学平台"
+    if isinstance(observers, basestring):
+        observers = observers.split(',')
+
+    text = '恭喜，已经完成计算，结果如下：\n'
+    text = text.encode('utf8') if isinstance(text, unicode) else text
+    msg = EmailMultiAlternatives(subject, text, settings.DEFAULT_FROM_EMAIL, observers)
+    html = render_calculate_result_as_html(sid)
+    html = html.encode('utf8') if isinstance(html, unicode) else html
+    msg.attach_alternative(html, 'text/html')
+    msg.send()
 
 
 @task()
@@ -152,13 +145,6 @@ def calculateTask(task, model):
 
     task.end_time = datetime.datetime.now()
     task.results = json.dumps(result)
-
-    try:
-        file_path = generate_pdf(id=task.pid, task_type=TASK_SINGLE)
-        task.result_pdf = File(open(file_path, "rb"))
-        task.save()
-    except Exception:
-        chemistry_logger.exception("failed to generate pdf")
 
     add_counter.delay(suite.sid)
 
